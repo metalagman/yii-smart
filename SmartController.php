7e5342ef567068a2d8eac2519dbe9445ae09f5fc
Alexey Samoylov <alexey.samoylov@gmail.com>
2014-08-19 18:22:50 +0800

    controller improvements

--- SmartController.php
+++ SmartController.php
@@ -13,4 +13,20 @@ class SmartController extends CController
 {
     /** @var array Controller breadcrumbs */
     public $breadcrumbs = [];
+
+    public function init()
+    {
+        parent::init();
+
+        $acceptType = Yii::app()->request->preferredAcceptType;
+        if ($acceptType['subType'] === 'json')
+            $this->disableWebLogs();
+    }
+
+    public function disableWebLogs()
+    {
+        foreach (Yii::app()->log->routes as $route)
+            if ($route instanceof CWebLogRoute || $route instanceof YiiDebugToolbarRoute)
+                $route->enabled = false;
+    }
 }
